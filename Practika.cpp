947576c5c5b2cde3f4257158b51b781f240a6eb1
Arina <arina.grivtsova@gmail.com>
2026-05-26 21:58:29 +0300

    updated practice

--- Practika.cpp
+++ Practika.cpp
@@ -6,8 +6,8 @@
  *
  * Programmer (1): Гривцова Арина Александровна
  *
- * Created: 01.05.2025
- * Last Revision: 09.05.2025
+ * Created: 01.05.2026
+ * Last Revision: 26.05.2026
  * Modified by:
  *
  * Comment:
@@ -16,275 +16,305 @@
  *
  *
  ******************************************************************/
-#include <iostream> 
-#include <fstream>  
-#include <cstring>  
 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> //ввод и вывод
+#include <fstream>//работа с файлами
+#include <cstring>//работа со строками
 using namespace std;
-const int SIZE = 100; // Максимальное количество записей о самолетах
 
-// Структура хранит данные об одном самолете
+//глобальные константы
+char FILE_NAME[50] = "Board.txt";
+
+const int TEXT_SIZE = 30;
+const int ERROR_SIZE = 200;
+
+
+//структура самолета
 struct Plane
 {
-    int flight;      // Номер рейса
-    char model[20];  // Марка летательного аппарата
-    char board[20];  // Бортовой номер самолета
-    int point;       // Пункт прибытия
+    int flight; //номер рейса
+    char model[TEXT_SIZE]; //модель самолета
+    char board[TEXT_SIZE]; //бортовой номер
+    int point; //пункт прибытия
+
+    bool correct; //корректность записи
+    char error[ERROR_SIZE]; //текст ошибки
 };
 
-// Функция выводит исходные данные до сортировки. Нужна для эхо-печати, чтобы показать, какие данные были считаны из файла.
-void outputBeforeSort(Plane planes[], int n);
-// Функция считывает данные из файла Board.txt. Также выполняет основные проверки входных данных.
-bool input(Plane planes[], int& n);
-// Функция создает индексный массив. В index записываются номера элементов массива planes: 0, 1 и т.д.
+//чтение количества записей
+bool readN(const char fileName[], int& n);
+//выделение памяти под структуры
+Plane* create(int n);
+//выделение памяти под индексный массив
+int* createIndex(int n);
+//освобождение памяти
+void clear(Plane* planes, int* index);
+//ввод данных из файла
+void input(const char fileName[], Plane planes[], int n);
+//создание индексного массива
 void makeIndex(int index[], int n);
-// Функция сортирует индексный массив методом пузырька.
-void sortIndex(Plane planes[], int index[], int n);
-// Функция выводит данные после сортировки. Вывод идет не напрямую из planes, а через индексный массив index.
-void output(Plane planes[], int index[], int n);
-// Функция проверяет, состоит ли строка только из цифр. Используется для проверки номера рейса, пункта прибытия и количества записей.
+//индексная сортировка
+void sort(Plane planes[], int index[], int n);
+//вывод таблицы до сортировки
+void printBefore(Plane planes[], int n);
+//вывод таблицы после сортировки
+void printAfter(Plane planes[], int index[], int n);
+//вывод ошибок
+void printErrors(Plane planes[], int n);
+//проверка строки на число
 bool isNumber(char s[]);
-// Функция проверяет, что строка не пустая. Используется для проверки марки самолета и бортового номера.
-bool isText(char s[]);
+//перевод строки в число
+int toInt(char s[]);
+//проверка бортового номера
+bool checkBoard(char board[]);
+//добавление ошибки
+void addError(Plane& plane, const char text[]);
+//проверка смысловых ошибок
+void checkErrors(Plane planes[], int n);
 
 int main()
 {
-    setlocale(LC_ALL, "Russian"); 
+    setlocale(LC_ALL, "Russian");
+    int n;
+    //тесты для проверки программы
+    strcpy(FILE_NAME, "Board.txt");
 
-    Plane planes[SIZE]; // Основной массив структур, где хранятся самолеты
-    int index[SIZE];    // Индексный массив для сортировки
-    int n;              // Количество записей в файле
+    //strcpy(FILE_NAME, "test_positive_01.txt");
+    //strcpy(FILE_NAME, "test_positive_02.txt");
+    //strcpy(FILE_NAME, "test_positive_03.txt");
+    //strcpy(FILE_NAME, "test_positive_04.txt");
+    //strcpy(FILE_NAME, "test_positive_05.txt");
 
-    // Если данные из файла считались без ошибок, то выполняется эхо-печать, создание индексов, сортировка и вывод.
-    if (input(planes, n))
+    //strcpy(FILE_NAME, "test_negative_01.txt");
+    //strcpy(FILE_NAME, "test_negative_02.txt");
+    //strcpy(FILE_NAME, "test_negative_03.txt");
+    //strcpy(FILE_NAME, "test_negative_04.txt");
+    //strcpy(FILE_NAME, "test_negative_05.txt");
+    //strcpy(FILE_NAME, "test_negative_06.txt");
+    //strcpy(FILE_NAME, "test_negative_07.txt");
+    //strcpy(FILE_NAME, "test_negative_08.txt");
+    //strcpy(FILE_NAME, "test_negative_09.txt");
+    //strcpy(FILE_NAME, "test_negative_10.txt");
+
+    //проверка количества записей
+    if (!readN(FILE_NAME, n))
     {
-        outputBeforeSort(planes, n); // Показываем данные до сортировки
-        makeIndex(index, n); // Создаем индексный массив
-        sortIndex(planes, index, n); // Сортируем только индексный массив
-        output(planes, index, n); // Выводим данные в отсортированном порядке
+        return 0;
     }
+
+    //динамическое выделение памяти
+    Plane* planes = create(n);
+    int* index = createIndex(n);
+    //ввод данных
+    input(FILE_NAME, planes, n);
+    //проверка смысловых ошибок
+    checkErrors(planes, n);
+    //вывод ошибок
+    printErrors(planes, n);
+    //эхо-печать
+    printBefore(planes, n);
+    //создание индексов
+    makeIndex(index, n);
+    //индексная сортировка
+    sort(planes, index, n);
+    //вывод после сортировки
+    printAfter(planes, index, n);
+    //очистка памяти
+    clear(planes, index);
 
     return 0;
 }
 
-// Функция проверяет, является ли строка числом
-bool isNumber(char s[])
+//чтение количества записей
+bool readN(const char fileName[], int& n)
 {
-    // Если строка пустая, значит это не число
-    if (strlen(s) == 0)
-    {
-        return false;
-    }
-    // Проверяем каждый символ строки
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        // Если символ не находится в диапазоне от '0' до '9', значит строка содержит не только цифры
-        if (s[i] < '0' || s[i] > '9')
-        {
-            return false;
-        }
-    }
-
-    // Если все символы являются цифрами, строка считается числом
-    return true;
-}
-
-// Функция проверяет, что строка не пустая
-bool isText(char s[])
-{
-    // strlen возвращает длину строки. Если длина равна 0, значит строка пустая.
-    if (strlen(s) == 0)
-    {
-        return false;
-    }
-
-    return true;
-}
-
-// Функция считывает данные из файла и проверяет их корректность
-bool input(Plane planes[], int& n)
-{
-    ifstream fin("Board.txt"); // Открываем файл для чтения
-
-    // Если файл не открылся, значит он отсутствует или находится не в той папке, откуда запускается программа.
+    ifstream fin(fileName);
+    //проверка открытия файла
     if (!fin)
     {
-        cout << "Ошибка: файл не найден" << endl;
+        cout << "ошибка: файл не найден" << endl;
         return false;
     }
 
-    char nStr[20]; // Количество записей сначала читаем как строку
-        fin >> nStr; // Читаем первую строку файла
+    char nStr[TEXT_SIZE];
+    fin >> nStr;
 
-    // Если чтение не удалось, значит файл пустой
+    //проверка пустого файла
     if (fin.fail())
     {
-        cout << "Ошибка: файл пустой" << endl;
+        cout << "ошибка: файл пустой" << endl;
         fin.close();
         return false;
     }
 
-    // Проверяем, что количество записей записано числом
+    //проверка количества записей
     if (!isNumber(nStr))
     {
-        cout << "Ошибка: количество записей должно быть числом" << endl;
+        cout << "ошибка: количество записей должно быть числом" << endl;
         fin.close();
         return false;
     }
-
-    // Переводим строку в число
-    n = atoi(nStr);
-
-    // Проверяем, что количество записей входит в допустимый диапазон
-    if (n <= 0 || n > SIZE)
+    n = toInt(nStr);
+    //проверка диапазона
+    if (n <= 0)
     {
-        cout << "Ошибка: некорректное количество записей" << endl;
+        cout << "ошибка: количество записей должно быть больше нуля" << endl;
         fin.close();
         return false;
     }
+    fin.close();
+    return true;
+}
 
-    char flightStr[20]; // Номер рейса временно читаем как строку
-    char pointStr[20];  // Пункт прибытия временно читаем как строку
+//выделение памяти под структуры
+Plane* create(int n)
+{
+    Plane* planes = new Plane[n];
+    return planes;
+}
 
-    // Считываем n записей из файла
+//выделение памяти под индексный массив
+int* createIndex(int n)
+{
+    int* index = new int[n];
+    return index;
+}
+
+//освобождение памяти
+void clear(Plane* planes, int* index)
+{
+    delete[] planes;
+    delete[] index;
+}
+
+//ввод данных из файла
+void input(const char fileName[], Plane planes[], int n)
+{
+    ifstream fin(fileName);
+
+    char temp[TEXT_SIZE];
+    //пропуск первой строки
+    fin >> temp;
+    //чтение записей
     for (int i = 0; i < n; i++)
     {
+        char flightStr[TEXT_SIZE];
+        char pointStr[TEXT_SIZE];
+
+        //начальные значения
+        planes[i].flight = 0;
+        planes[i].point = 0;
+        planes[i].model[0] = '\0';
+        planes[i].board[0] = '\0';
+        planes[i].correct = true;
+        planes[i].error[0] = '\0';
+
         fin >> flightStr;
         fin >> planes[i].model;
         fin >> planes[i].board;
         fin >> pointStr;
 
-        // Если не удалось считать все 4 поля, значит в записи не хватает данных.
+        //проверка чтения строки
         if (fin.fail())
         {
-            cout << "Ошибка: недостаточно данных в записи № " << i + 1 << endl;
-            fin.close();
-            return false;
+            addError(planes[i], "недостаточно данных");
+            fin.clear();
+            break;
         }
 
-        // Проверяем номер рейса. Он должен состоять только из цифр.
+        //проверка номера рейса
         if (!isNumber(flightStr))
         {
-            cout << "Ошибка: номер рейса в записи № "
-                << i + 1 << " должен быть числом" << endl;
-
-            fin.close();
-            return false;
+            addError(planes[i], "номер рейса не число");
         }
-
-        // Проверяем марку самолета. Она не должна быть пустой.
-        if (!isText(planes[i].model))
+        else
         {
-            cout << "Ошибка: пустая марка самолета в записи № "
-                << i + 1 << endl;
-
-            fin.close();
-            return false;
+            planes[i].flight = toInt(flightStr);
+            if (planes[i].flight <= 0)
+            {
+                addError(planes[i], "номер рейса <= 0");
+            }
         }
 
-        // Проверяем бортовой номер. Он не должен быть пустым.
-        if (!isText(planes[i].board))
+        //проверка модели
+        if (strlen(planes[i].model) == 0)
         {
-            cout << "Ошибка: пустой бортовой номер в записи № "
-                << i + 1 << endl;
-
-            fin.close();
-            return false;
+            addError(planes[i], "пустая модель");
         }
 
-        // Проверяем пункт прибытия. Он тоже должен быть числом.
+        //проверка бортового номера
+        if (!checkBoard(planes[i].board))
+        {
+            addError(planes[i], "неверный бортовой номер");
+        }
+
+        //проверка пункта прибытия
         if (!isNumber(pointStr))
         {
-            cout << "Ошибка: пункт прибытия в записи № "
-                << i + 1 << " должен быть числом" << endl;
-
-            fin.close();
-            return false;
+            addError(planes[i], "пункт прибытия не число");
         }
-
-        // После проверок переводим строки в числа
-        planes[i].flight = atoi(flightStr);
-        planes[i].point = atoi(pointStr);
-
-        // Номер рейса не может быть отрицательным или равным нулю
-        if (planes[i].flight <= 0)
+        else
         {
-            cout << "Ошибка: номер рейса должен быть положительным" << endl;
-
-            fin.close();
-            return false;
-        }
-
-        // Пункт прибытия тоже должен быть положительным числом
-        if (planes[i].point <= 0)
-        {
-            cout << "Ошибка: пункт прибытия должен быть положительным" << endl;
-
-            fin.close();
-            return false;
+            planes[i].point = toInt(pointStr);
+            if (planes[i].point <= 0)
+            {
+                addError(planes[i], "пункт прибытия <= 0");
+            }
         }
     }
-
-    fin.close(); 
-        return true;
+    fin.close();
 }
 
-// Функция создает индексный массив
+//создание индексного массива
 void makeIndex(int index[], int n)
 {
-    // Каждый элемент index хранит номер соответствующей записи в planes
     for (int i = 0; i < n; i++)
     {
         index[i] = i;
     }
 }
 
-// Функция выполняет индексную сортировку методом пузырька
-void sortIndex(Plane planes[], int index[], int n)
+//индексная сортировка
+void sort(Plane planes[], int index[], int n)
 {
-    // Внешний цикл отвечает за количество проходов
     for (int i = 0; i < n - 1; i++)
     {
-        // Внутренний цикл сравнивает соседние элементы
         for (int j = 0; j < n - i - 1; j++)
         {
-            // Сравниваются номера рейсов через индексный массив. То есть мы смотрим не planes[j], а planes[index[j]].
-            if (planes[index[j]].flight >
-                planes[index[j + 1]].flight)
+            int left = index[j];
+            int right = index[j + 1];
+            //некорректные записи остаются в начале
+            if (!planes[left].correct && planes[right].correct)
             {
-                // Если порядок неправильный, то меняем местами только индексы.
+                continue;
+            }
+
+            if (planes[left].correct && !planes[right].correct)
+            {
                 int temp = index[j];
                 index[j] = index[j + 1];
                 index[j + 1] = temp;
+            }
+            else
+            {
+                if (planes[left].flight > planes[right].flight)
+                {
+                    int temp = index[j];
+                    index[j] = index[j + 1];
+                    index[j + 1] = temp;
+                }
             }
         }
     }
 }
 
-// Функция выводит данные после сортировки
-void output(Plane planes[], int index[], int n)
+//вывод таблицы до сортировки
+void printBefore(Plane planes[], int n)
 {
-    cout << "Рейсы после сортировки:\n\n";
+    cout << "таблица до сортировки:" << endl;
 
-    // Вывод идет в порядке, который хранится в index
-    for (int i = 0; i < n; i++)
-    {
-        // k — это номер элемента в массиве planes
-        int k = index[i];
-
-        cout << planes[k].flight << " ";
-        cout << planes[k].model << " ";
-        cout << planes[k].board << " ";
-        cout << planes[k].point << endl;
-    }
-}
-
-// Функция выводит данные в исходном порядке
-void outputBeforeSort(Plane planes[], int n)
-{
-    cout << "Рейсы до сортировки:\n\n";
-
-    // Здесь вывод идет напрямую из массива planes, поэтому порядок такой же, как в файле.
     for (int i = 0; i < n; i++)
     {
         cout << planes[i].flight << " ";
@@ -294,4 +324,164 @@ void outputBeforeSort(Plane planes[], int n)
     }
 
     cout << endl;
+}
+
+//вывод таблицы после сортировки
+void printAfter(Plane planes[], int index[], int n)
+{
+    cout << "таблица после сортировки:" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = index[i];
+        cout << planes[k].flight << " ";
+        cout << planes[k].model << " ";
+        cout << planes[k].board << " ";
+        cout << planes[k].point;
+        //пометка некорректной записи
+        if (!planes[k].correct)
+        {
+            cout << " [ошибка]";
+        }
+        cout << endl;
+    }
+}
+
+//вывод ошибок
+void printErrors(Plane planes[], int n)
+{
+    bool hasErrors = false;
+    cout << "ошибки:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if (!planes[i].correct)
+        {
+            hasErrors = true;
+            cout << "запись ";
+            cout << i + 1;
+            cout << ": ";
+            cout << planes[i].error;
+            cout << endl;
+        }
+    }
+
+    //если ошибок нет
+    if (!hasErrors)
+    {
+        cout << "ошибок нет" << endl;
+    }
+
+    cout << endl;
+}
+
+//проверка строки на число
+bool isNumber(char s[])
+{
+    //пустая строка
+    if (strlen(s) == 0)
+    {
+        return false;
+    }
+    //проверка всех символов
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] < '0' || s[i] > '9')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//перевод строки в число
+int toInt(char s[])
+{
+    int number = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        number = number * 10 + (s[i] - '0');
+    }
+    return number;
+}
+
+//проверка бортового номера
+bool checkBoard(char board[])
+{
+    //длина должна быть 6:
+    //B-1234
+    if (strlen(board) != 6)
+    {
+        return false;
+    }
+    //проверка буквы
+    if (board[0] != 'B')
+    {
+        return false;
+    }
+    //проверка дефиса
+    if (board[1] != '-')
+    {
+        return false;
+    }
+    //проверка цифр
+    for (int i = 2; i < 6; i++)
+    {
+        if (board[i] < '0' || board[i] > '9')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//добавление ошибки
+void addError(Plane& plane, const char text[])
+{
+    plane.correct = false;
+    int i = strlen(plane.error);
+    if (i != 0)
+    {
+        plane.error[i] = ';';
+        plane.error[i + 1] = ' ';
+        i = i + 2;
+    }
+    int j = 0;
+    while (text[j] != '\0' && i < ERROR_SIZE - 1)
+    {
+        plane.error[i] = text[j];
+        i++;
+        j++;
+    }
+    plane.error[i] = '\0';
+}
+
+//проверка смысловых ошибок
+void checkErrors(Plane planes[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            //одинаковые рейсы
+            if (planes[i].flight == planes[j].flight
+                && planes[i].flight != 0)
+            {
+                addError(planes[i], "повтор рейса");
+                addError(planes[j], "повтор рейса");
+            }
+            //один борт - разные модели
+            if (strcmp(planes[i].board,
+                planes[j].board) == 0)
+            {
+                if (strcmp(planes[i].model,
+                    planes[j].model) != 0)
+                {
+                    addError(planes[i],
+                        "один борт у разных моделей");
+                    addError(planes[j],
+                        "один борт у разных моделей");
+                }
+            }
+        }
+    }
 }
